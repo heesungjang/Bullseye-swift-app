5f24d186bd6cf930dd49a820859578e16f7abcb5
heesungjang <heesungj7@gmail.com>
2022-01-04 02:35:37 +0900

    Initial Commit

---
+++ Shared/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by heesungjang on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
