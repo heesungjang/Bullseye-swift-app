f765336ff20ec8418161c8e85c52d54e8ad29e03
heesungjang <heesungj7@gmail.com>
2022-01-05 00:51:07 +0900

    first modal struct + state binding in swift

---
+++ Bullseye/Modals/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye (iOS)
+//
+//  Created by heesungjang on 2022/01/05.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 42
+    var score: Int = 0
+    var round: Int = 1
+    
+    func points(sliderValue:Int) -> Int{
+        return 999
+    }
+}
--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     
     @State private var alertsIsVisible: Bool = false
     @State private var sliderValue: Double = 50.0
-    
+    @State private var game: Game = Game()
     
     var body: some View {
         VStack {
@@ -21,7 +21,7 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .lineSpacing(4.0)
                 .font(.footnote)
-            Text("89")
+            Text(String(game.target))
                 .kerning(-1.0)
                 .font(.largeTitle)
                 .fontWeight(.black)
@@ -41,7 +41,7 @@ struct ContentView: View {
                 
                 var roundedValue: Int = Int(self.sliderValue.rounded())
                 
-                return Alert(title:Text("Hello there!"),message: Text("The slider value is \(roundedValue)"), dismissButton: .default(Text("Awesome!")))
+                return Alert(title:Text("Hello there!"),message: Text("The slider value is \(roundedValue). \n" + "You scored \(self.game.points(sliderValue:roundedValue)) points this round."), dismissButton: .default(Text("Awesome!")))
             })
         }
     }
